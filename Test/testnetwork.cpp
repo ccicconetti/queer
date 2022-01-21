514c5b68f311d956e8375c0ceb79f84bef0e90f7
Claudio Cicconetti <cicconetti@gmail.com>
2022-01-21 18:23:44 +0100

    Initial experiments with boost::graph

--- Test/testnetwork.cpp
+++ Test/testnetwork.cpp
@@ -31,14 +31,96 @@ SOFTWARE.
 
 #include "QuantumRouting/network.h"
 
+#include "Details/examplenetwork.h"
+
+#include <boost/filesystem.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/detail/adjacency_list.hpp>
+#include <boost/graph/graph_selectors.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/graphml.hpp>
+#include <boost/property_map/dynamic_property_map.hpp>
+#include <boost/property_map/property_map.hpp>
+
+#include <glog/logging.h>
+
+#include <fstream>
+
 #include "gtest/gtest.h"
 
 namespace uiiit {
 namespace qr {
 
-struct TestNetwork : public ::testing::Test {};
+struct TestNetwork : public ::testing::Test {
+  TestNetwork()
+      : theFilename(
+            (boost::filesystem::current_path() / "example.gml").string()) {
+    // noop
+  }
 
-TEST_F(TestNetwork, test_ctor) { ASSERT_NO_THROW(Network()); }
+  void createExampleNetworkFile() const {
+    std::ofstream myOutfile(theFilename);
+    myOutfile << exampleNetwork();
+  }
+
+  void SetUp() {
+    boost::filesystem::remove(theFilename);
+  }
+
+  void TearDown() {
+    if (not VLOG_IS_ON(1)) {
+      boost::filesystem::remove(theFilename);
+    }
+  }
+
+  const std::string theFilename;
+};
+
+TEST_F(TestNetwork, test_ctor) {
+  ASSERT_NO_THROW(Network());
+}
+
+TEST_F(TestNetwork, test_read_graphml) {
+  createExampleNetworkFile();
+
+  struct GraphData {
+    std::string theLabel;
+    double      theLongitude;
+    double      theLatitude;
+  };
+
+  using Graph =
+      boost::adjacency_list<boost::listS,
+                            boost::vecS,
+                            boost::undirectedS,
+                            GraphData,
+                            boost::property<boost::edge_weight_t, float>,
+                            boost::no_property,
+                            boost::listS>;
+
+  Graph myGraph;
+
+  boost::dynamic_properties myDp(boost::ignore_other_properties);
+  myDp.property("label", boost::get(&GraphData::theLabel, myGraph));
+  myDp.property("Latitude", boost::get(&GraphData::theLatitude, myGraph));
+  myDp.property("Longitude", boost::get(&GraphData::theLongitude, myGraph));
+
+  std::ifstream myInfile(theFilename);
+  boost::read_graphml(myInfile, myGraph, myDp, 0);
+
+  EXPECT_EQ(61, boost::num_vertices(myGraph));
+  EXPECT_EQ(89, boost::num_edges(myGraph));
+
+  const auto myVertices = boost::vertices(myGraph);
+  for (auto it = myVertices.first; it != myVertices.second; ++it) {
+    LOG(INFO) << "vertex " << *it << "["
+              << boost::get(&GraphData::theLabel, myGraph, *it) << "]"
+              << ", out degree " << boost::out_degree(*it, myGraph);
+  }
+
+  boost::print_graph(myGraph, boost::get(&GraphData::theLabel, myGraph));
+}
 
 } // namespace qr
 } // namespace uiiit
