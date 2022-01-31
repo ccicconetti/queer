af21aaf12f52c047f902a8e154318ada5cbd6a02
Claudio Cicconetti <cicconetti@gmail.com>
2022-01-31 15:58:27 +0100

    Experiment 001: add script

---
+++ Experiments/001_Constant_Rate_PPP/var-flows/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+MAIN=../main-001
+
+if [[ "$DRY" == "" && ! -d "data" ]] ; then
+  mkdir data 2> /dev/null
+fi
+
+if [ ! -x $MAIN ] ; then
+  echo "missing executable: $MAIN"
+  exit 1
+fi
+
+flows="10 20 50 100 200 500 1000 2000 5000 10000"
+
+for f in $flows ; do
+  cmd="$MAIN \
+    --num-threads 100 \
+    --output data/out-$f.csv \
+    --seed-start 0 \
+    --seed-end 999 \
+    --mu 50 \
+    --link-min-epr 1 \
+    --link-max-epr 100 \
+    --num-flows $f \
+    --rate-min 1 \
+    --rate-max 10"
+
+  if [ "$DRY" != "" ] ; then
+    echo $cmd
+  else
+    GLOG_v=$VERBOSE $cmd
+  fi
+done
