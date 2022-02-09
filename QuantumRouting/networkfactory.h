5296c80f595beae11c3bc9fe348de8927ce74375
Claudio Cicconetti <cicconetti@gmail.com>
2022-02-09 09:38:47 +0100

    Add Experiment 002 main
    Create a new compilation unit (networkfactory)

---
+++ QuantumRouting/networkfactory.h
@@ -0,0 +1,51 @@
+/*
+              __ __ __
+             |__|__|  | __
+             |  |  |  ||__|
+  ___ ___ __ |  |  |  |
+ |   |   |  ||  |  |  |    Ubiquitous Internet @ IIT-CNR
+ |   |   |  ||  |  |  |    C++ quantum routing libraries and tools
+ |_______|__||__|__|__|    https://github.com/ccicconetti/quantum-routing
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>
+Copyright (c) 2022 C. Cicconetti <https://ccicconetti.github.io/>
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include "QuantumRouting/capacitynetwork.h"
+
+#include <memory>
+
+namespace uiiit {
+namespace qr {
+
+std::unique_ptr<CapacityNetwork>
+makeCapacityNetworkPpp(const double      aLinkMinEpr,
+                       const double      aLinkMaxEpr,
+                       const std::size_t aSeed,
+                       const double      aMu,
+                       const double      aGridLength,
+                       const double      aThreshold,
+                       const double      aLinkProbability);
+
+} // namespace qr
+} // namespace uiiit
