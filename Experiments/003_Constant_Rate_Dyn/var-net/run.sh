d62aed1fc6cc8f4c6d5a177568ef6d58e681d028
Claudio Cicconetti <cicconetti@gmail.com>
2022-04-11 11:28:02 +0200

    Experiment 003: update var-net

--- Experiments/003_Constant_Rate_Dyn/var-net/run.sh
+++ Experiments/003_Constant_Rate_Dyn/var-net/run.sh
@@ -19,53 +19,57 @@ if [ ! -x $MAIN ] ; then
   exit 1
 fi
 
-maxlinkrates="80 120 160 200 240 280 320 360 400"
+srcdstpolicies="uniform nodecapacities"
+maxlinkrates="100 150 200 250 300 350 400 450 500"
 numnodes="40 60 80 100 120 140 160 180 200"
 rates="1 10"
 fidelities="0.7 0.9"
 
-for m in $maxlinkrates ; do
-  for n in $numnodes ; do
-    for r in $rates ; do
-      for f in $fidelities ; do
-        output=data/out-$m-$n-$r-$f.csv
-        cmd="$MAIN \
-          --output $output \
-          --num-threads $CONCURRENCY \
-          --seed-start 0 \
-          --seed-end 100 \
-          --mu $n \
-          --link-min-epr 1 \
-          --link-max-epr $m \
-          --grid-size 100000 \
-          --threshold 15000 \
-          --sim-duration 100 \
-          --warmup-duration 10 \
-          --arrival-rate 100 \
-          --flow-duration 10 \
-          --net-epr-rates $r \
-          --q 0.5 \
-          --fidelity-init 0.95 \
-          --fidelity-threshold $f \
-          "
+for p in $srcdstpolicies ; do
+  for m in $maxlinkrates ; do
+    for n in $numnodes ; do
+      for r in $rates ; do
+        for f in $fidelities ; do
+          output=data/out-$m-$n-$r-$f.csv
+          cmd="$MAIN \
+            --output $output \
+            --num-threads $CONCURRENCY \
+            --seed-start 0 \
+            --seed-end 100 \
+            --mu $n \
+            --link-min-epr 1 \
+            --link-max-epr $m \
+            --src-dst-policy $p \
+            --grid-size 100000 \
+            --threshold 15000 \
+            --sim-duration 100 \
+            --warmup-duration 10 \
+            --arrival-rate 100 \
+            --flow-duration 10 \
+            --net-epr-rates $r \
+            --q 0.5 \
+            --fidelity-init 0.95 \
+            --fidelity-threshold $f \
+            "
 
-        if [ "$EXPLAIN" != "" ] ; then
-          $cmd --explain
-          exit 1
+          if [ "$EXPLAIN" != "" ] ; then
+            $cmd --explain
+            exit 1
 
-        elif [ "$DRY" != "" ] ; then
-          echo $cmd
+          elif [ "$DRY" != "" ] ; then
+            echo $cmd
 
-        else
-          now=$(date)
-          echo -n "$now $output."
-          if [ -r $output ] ; then
-            echo ".skipped"
           else
-            GLOG_v=$VERBOSE $cmd
-            echo ".done"
+            now=$(date)
+            echo -n "$now $output."
+            if [ -r $output ] ; then
+              echo ".skipped"
+            else
+              GLOG_v=$VERBOSE $cmd
+              echo ".done"
+            fi
           fi
-        fi
+        done
       done
     done
   done
