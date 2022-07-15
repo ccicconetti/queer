e9b74e73ae3eb1d5a10efee38f77580bf8a3f7f0
Claudio Cicconetti <cicconetti@gmail.com>
2022-07-15 16:13:32 +0200

    Add plot of fidelity purification vs. round

--- Scripts/fidelity_length.py
+++ Scripts/fidelity_length.py
@@ -6,8 +6,17 @@ import math
 def l_i(F_i: float, F: float):
     return math.floor(math.log((4 * F_i - 1) / 3) / math.log((4 * F - 1) / 3))
 
+
 def purif(x1: float, x2: float):
     return x1 * x2 / (x1 * x2 + (1 - x1) * (1 - x2))
+
+
+def purif_round(x: float, n: int):
+    assert n >= 1
+    ret = x
+    for r in range(n):
+        ret = purif(x, ret)
+    return ret
 
 
 if __name__ == "__main__":
@@ -21,3 +30,10 @@ if __name__ == "__main__":
     print(purif(0.96, 0.98))
     print(purif(0.92, 0.98))
     print(purif(0.93, 0.97))
+
+    with open("fidelity_round.dat", "w") as outfile:
+        for f in range(50, 101, 1):
+            values = []
+            for r in range(1, 5, 1):
+                values.append(f"{purif_round(f / 100.0, r)}")
+            outfile.write(f'{f/100.0} {" ".join(values)}\n')
