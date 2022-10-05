c378e1ce0d21e05797b6858254b681d609c94660
Claudio Cicconetti <cicconetti@gmail.com>
2022-10-05 16:38:17 +0200

    Experiment 004: add run script

---
+++ Experiments/004_Multiple_Classes_PPP/multi-prio/run.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+MAIN=../main-004
+
+if [[ "$DRY" == "" && ! -d "data" ]] ; then
+  mkdir data 2> /dev/null
+fi
+
+if [ ! -x $MAIN ] ; then
+  echo "missing executable: $MAIN"
+  exit 1
+fi
+
+topos="dense sparse"
+algos="random bestfit drr"
+numapps="10 20 50 100 200 500 1000"
+
+for t in $topos ; do
+for a in $algos ; do
+for n in $numapps ; do
+
+  if [ $t == "dense" ] ; then
+    mu=100
+    threshold=20000
+  elif [ $t == "sparse" ] ; then
+    mu=50
+    threshold=15000
+  else
+    echo "invalid topology type: $t"
+    exit 1
+  fi
+
+  output=out-$t-$a-$n
+  cmd="$MAIN \
+    --num-threads 0 \
+    --seed-start 0 \
+    --seed-end 5000 \
+    --mu $mu \
+    --link-min-epr 1 \
+    --link-max-epr 400 \
+    --num-apps $n \
+    --num-peers-min 2 \
+    --num-peers-max 4 \
+    --distance-min 2 \
+    --distance-max 7 \
+    --grid-size 60000 \
+    --threshold $threshold \
+    --link-probability 0.5 \
+    --algorithm $a \
+    --q 0.5 \
+    --quantum 10 \
+    --k 4 \
+    --fidelity-init 0.95 \
+    --priorities 4,2,1 \
+    --fidelity-thresholds 0.7 \
+    --target-residual -1 \
+    --output data/$output.csv \
+    "
+
+  if [ "$DRY" != "" ] ; then
+    echo $cmd
+  else
+    GLOG_v=$VERBOSE $cmd
+  fi
+
+done
+done
+done
