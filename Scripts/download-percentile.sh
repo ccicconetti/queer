337afc99d8a67c64dc5eea2d2b98d956a328df7b
Claudio Cicconetti <cicconetti@gmail.com>
2022-10-06 11:07:36 +0200

    Experiment 004: add post script
    Also separate the percentile.py download from the script

---
+++ Scripts/download-percentile.sh
@@ -0,0 +1,7 @@
+  if [ ! -x percentile.py ] ; then
+    curl -opercentile.py https://raw.githubusercontent.com/ccicconetti/serverlessonedge/master/scripts/percentile.py >& /dev/null
+    if [ $? -ne 0 ] ; then
+      echo "error downloading the percentile.py script"
+    fi
+    chmod 755 percentile.py
+  fi
