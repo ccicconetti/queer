c6dee0cccf5c376ebaed60c08dbe3b54dd000418
Claudio Cicconetti <cicconetti@gmail.com>
2022-11-16 17:23:45 +0100

    Update collect.sh script

--- Scripts/collect.sh
+++ Scripts/collect.sh
@@ -5,16 +5,28 @@ if [ "$SERVERS" == "" ] ; then
   exit 1
 fi
 
-if [ -d data ] ; then
-  read -n 1 -p "Do you want to remove the directory 'data'? (Ctrl+C to abort)"
-  rm -rf data >& /dev/null
+if [ "$DATADIR" == "" ] ; then
+  DATADIR=data
+fi
+
+if [ -d $DATADIR ] ; then
+  read -n 1 -p "There is already a directory '$DATADIR', do you want to proceed? (Ctrl+C to abort)"
+else
+  mkdir $DATADIR
+fi
+
+if [[ -d $DATADIR && "$CLEANDIR" != "" ]] ; then
+  rm -rf $DATADIR/* >& /dev/null
 fi
 
 for i in $SERVERS ; do
-  scp -r $i:$PWD/data data-$i
+  scp -r $i:$PWD/$DATADIR $DATADIR-$i
 done
 
-mkdir data 2> /dev/null
-for i in $(cd data-$(echo $SERVERS | cut -f 1 -d ' ') && ls -1) ; do
-  cat data-*/$i > data/$i
+for i in $(cd $DATADIR-$(echo $SERVERS | cut -f 1 -d ' ') && ls -1) ; do
+  cat $DATADIR-*/$i > $DATADIR/$i
+done
+
+for i in $SERVERS ; do
+  rm -rf $DATADIR-$i
 done
