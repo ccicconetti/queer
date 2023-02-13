d5b1289266ea7d773b57eb92bf1a2dd6a990e419
Claudio Cicconetti <cicconetti@gmail.com>
2023-02-13 10:51:10 +0100

    Add PRINT_ONLY option to download-artifacts.sh

--- Scripts/download-artifacts.sh
+++ Scripts/download-artifacts.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-if [ -d data ] ; then
-  echo "data directory already present: cowardly refusing to proceed"
-  exit 1
-fi
-
 pushd .. > /dev/null
 experiment=$(basename $PWD)
 popd > /dev/null
 sub=$(basename $PWD)
+
+if [ "$PRINT_ONLY" != "" ] ; then
+  echo $experiment-$sub.tgz
+  exit 0
+fi
+
+if [ -d data ] ; then
+  echo "data directory already present: cowardly refusing to proceed"
+  exit 1
+fi
 
 mkdir data 2> /dev/null
 wget http://turig.iit.cnr.it/~claudio/public/quantum-routing/$experiment-$sub.tgz -O- | tar zx
